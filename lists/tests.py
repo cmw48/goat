658af7a6dbe9516d01d4a18e0d783764b947ae58
Chris Westling <cmwestling@gmail.com>
2018-03-30 02:12:47 -0400

    refactor home page view to use a template and clean up tests

--- lists/tests.py
+++ lists/tests.py
@@ -9,14 +9,25 @@ class HomePageTest(TestCase):
     def test_physics(self):
         self.assertEqual(1+1, 2)
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+# commented code replaced by use of Django Test Client and templates
+#    def test_root_url_resolves_to_home_page_view(self):
+#        found = resolve('/')
+#        self.assertEqual(found.func, home_page)
+
+#    def test_home_page_returns_correct_html(self):
+#        request = HttpRequest()
+#        response = home_page(request)
+#        html = response.content.decode('utf8')
+#        self.assertTrue(html.startswith('<html>'))
+#        self.assertIn('<title>To-Do lists</title>', html)
+#        self.assertTrue(html.strip().endswith('</html>'))
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
