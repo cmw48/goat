c9955aaf89626b9908089ffea5d41b428bf2a5e1
Chris Westling <cmwestling@gmail.com>
2018-03-30 11:12:22 -0400

    changed view with if statement

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-#from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    #return HttpResponse('<html><title>To-Do lists</title></html>')
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
