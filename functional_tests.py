582946d3d34ddba509dc40d52f4b85dbd59faf4f
Chris Westling <cmwestling@gmail.com>
2018-04-25 15:03:46 -0400

    small atomic change

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #user navigates to homepage
         self.browser.get('http://localhost:8080')
@@ -34,10 +39,11 @@ class NewVisitorTest(unittest.TestCase):
         # when enter is pressed, page updates
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
+        self.check_for_row_in_list_table('1: here is the first list item.')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: here is the first list item.', [row.text for row in rows])
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn('1: here is the first list item.', [row.text for row in rows])
         #self.assertTrue(
         #    any(row.text == '1. here is the first list item.' for row in rows),
         #    f"New to-do item did not appear in table.  Contents were:\n{table.text}")
@@ -50,9 +56,11 @@ class NewVisitorTest(unittest.TestCase):
         # when enter is pressed, page updates
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: here is a second list item.', [row.text for row in rows])
+        ##table = self.browser.find_element_by_id('id_list_table')
+        ##rows = table.find_elements_by_tag_name('tr')
+        ##self.assertIn('2: here is a second list item.', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: here is the first list item.')
+        self.check_for_row_in_list_table('2: here is a second list item.')
 
         # page updates, and both list items are displayed
         self.fail('Finish the test!')
