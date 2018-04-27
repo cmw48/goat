cf6c941bfe221823c17ca4bbf9a984d47e65fd3c
Chris Westling <cmwestling@gmail.com>
2018-04-26 22:42:39 -0400

    first ever model

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 # Create your models here.
+# remember to makemigrations each time you change models
 class Item(models.Model):
     text = models.TextField(default='')
